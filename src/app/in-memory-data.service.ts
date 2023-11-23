ec2aceec2ea62689b74f829d38be85419dcddf26
Jamil Alizada <xalizadax@gmail.com>
2023-11-23 12:35:36 +0400

    careers id change

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -156,19 +156,19 @@ export class InMemoryDataService implements InMemoryDbService {
 
     let careers = [
       {
-        id: 2,
+        id: 0,
         name: 'Geophysicist\n(Interpreter)',
         location: 'Baku, Azerbaijan',
         link: 'https://forms.clickup.com/31136212/f/xp6em-2783/J3LGWHBCC3OFPAJZK8'
       },
       {
-        id: 8,
+        id: 1,
         name: 'Projects’ Manager /\nCoordinator',
         location: 'Baku, Azerbaijan',
         link: 'https://forms.clickup.com/31136212/f/xp6em-3244/1QNX0XETN9ZX8PE5J3'
       },
       {
-        id: 9,
+        id: 2,
         name: 'Algorithm’s\nDeveloper',
         location: 'Baku, Azerbaijan',
         link: 'https://forms.clickup.com/31136212/f/xp6em-3204/YYUDA6QOPEIW1M5GQE'
