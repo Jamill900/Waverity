6fa46b8e1bf362064ee2d9a6e2e0e9fcdc09f09e
Jamil <xalizadax@gmail.com>
2024-01-08 15:04:34 +0400

    careers new link

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -255,7 +255,7 @@ export class InMemoryDataService implements InMemoryDbService {
         id: 2,
         name: 'Algorithm’s Developer\nfor Geophysical Products',
         location: 'Baku, Azerbaijan',
-        link: 'https://forms.clickup.com/31136212/f/xp6em-3204/YYUDA6QOPEIW1M5GQE'
+        link: 'https://forms.clickup.com/31136212/f/xp6em-3307/M63OT65W62EK4RTEJS'
       }
     ];
 
